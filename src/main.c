77756beefff416bbcf93bdeb3d17ad3432df0fd8
David Lane <david.lane@canonical.com>
2023-07-11 12:06:17 +1000

    works

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
@@ -10,38 +11,37 @@ int main(int argc, char **argv)
     fd = open("/home/dclane/demofile", O_RDONLY);
     if (!(fd > 0)) {
         printf("problem opening /home/dclane/demofile\n");
-        return 0;
     } else {
         printf("opened /home/dclane/demofile ok :)\n");
+        close(fd);
     }
-    close(fd);
     fd = 0;
 
     fd = open("/etc/passwd", O_RDONLY);
     if (!(fd > 0)) {
         printf("problem opening /etc/passwd\n");
-        return 0;
     } else {
         printf("opened /etc/passwd ok :)\n");
+        close(fd);
     }
-    close(fd);
     fd = 0;
 
     fd = open("/etc/ssl/certs/java/cacerts", O_RDONLY);
     if (!(fd > 0)) {
         printf("problem opening /etc/ssl/certs/java/cacerts\n");
-        return 0;
     } else {
         printf("opened /etc/ssl/certs/java/cacerts ok :)\n");
+        close(fd);
     }
-    close(fd);
     fd = 0;
 
-    const char* snap_user_data = getenv("SNAP_USER_DATA");
-    const char* snap_user_common = getenv("SNAP_USER_DATA");
+    printf("working\n");
 
-    printf("%s", snap_user_data);
-    printf("%s", snap_user_common);
+    const char* snap_user_data = getenv("SNAP_USER_DATA");
+    const char* snap_user_common = getenv("SNAP_USER_COMMON");
+
+    printf("SNAP_USER_DATA: %s\n", snap_user_data);
+    printf("SNAP_USER_COMMON: %s\n", snap_user_common);
     
     return 0;
 
