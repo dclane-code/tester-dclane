a369a32b591282ea8b4e2037b1b072fe877066de
David Lane <david.lane@canonical.com>
2023-07-13 12:33:43 +1000

    plugging docker, doing nothing else

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -f tester.tgz
-tar czvf tester.tgz src/main.c src/Makefile
-snapcraft clean tester-dclane
-snapcraft
-rm -rf squashfs-root
-unsquashfs ./tester-dclane_*.snap
-snap try squashfs-root
--- src/main.c
+++ src/main.c
@@ -5,38 +5,6 @@
 
 int main(int argc, char **argv)
 {
-    int fd = 0;
-
-    printf("layout / interface tester\n");
-    fd = open("/home/dclane/demofile", O_RDONLY);
-    if (!(fd > 0)) {
-        printf("problem opening /home/dclane/demofile\n");
-    } else {
-        printf("opened /home/dclane/demofile ok :)\n");
-        close(fd);
-    }
-    fd = 0;
-
-    fd = open("/etc/passwd", O_RDONLY);
-    if (!(fd > 0)) {
-        printf("problem opening /etc/passwd\n");
-    } else {
-        printf("opened /etc/passwd ok :)\n");
-        close(fd);
-    }
-    fd = 0;
-
-    fd = open("/etc/ssl/certs/java/cacerts", O_RDONLY);
-    if (!(fd > 0)) {
-        printf("problem opening /etc/ssl/certs/java/cacerts\n");
-    } else {
-        printf("opened /etc/ssl/certs/java/cacerts ok :)\n");
-        close(fd);
-    }
-    fd = 0;
-
-    printf("working\n");
-
     const char* snap_user_data = getenv("SNAP_USER_DATA");
     const char* snap_user_common = getenv("SNAP_USER_COMMON");
 
