a26a34c16b6bb39b4ac569943e0fbe7241ba90ea
David Lane <david.lane@canonical.com>
2023-08-23 15:07:58 +1000

    added 'compiler'

---
+++ compiler/comp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#this needs to take the value from useful snap's input.h (4) and return double it
+import os
+
+print("{}".format(os.getcwd()))
+
+print(os.environ)
+
+#if os.path.exists(os.environ['CRAFT_PART_INSTALL']):
+    #print("found the install directory")
+#else:
+    #print("didn't find the install directory")
+
+
+
